0671f47cff0f9bf03039228c2dcc9aea366abed5
tbowen8615 <tbowen8615@gmail.com>
2024-12-11 10:07:57 -0500

    prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22', 'Oatmeal - $21', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $25', 'Pasta - $20', 'Burger - $15', 'Salmon - $25'];
+const dessertMenu = ['Cake - $8', 'Ice Cream - $8', 'Pudding - $8', 'Fruit Salad - $8'];
 
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
